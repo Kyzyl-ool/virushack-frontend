5d0fc06d4d0ef7b4559ad59f3f149d8f69ac8701
aleksandrsokolov <timac11@bk.ru>
2020-05-04 20:19:45 +0300

    fix temperature

--- lib/pages/SymptomsPage/index.tsx
+++ lib/pages/SymptomsPage/index.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import { Button, Select, Slider } from 'antd';
+import React, { useState } from 'react';
+import { Button, Input, Select, Slider, Typography } from 'antd';
 import './SymptomsPage.scss';
-import { Typography } from 'antd';
 
 const { Title, Text } = Typography;
 const { Option } = Select;
@@ -23,45 +22,55 @@ const places: string[] = [
 const degrees: string[] = ['колющая', 'давящая', 'режущая', 'другое'];
 
 export const SymptomsPage: React.FC = props => {
+  const [param, setParam] = useState('Боль');
+
   return (
     <div className="symptoms-page__container">
       <Title level={2}>Симптомы</Title>
       <div className="symptoms-page__selector-container">
         <div className="symptoms-page__selector-container-item">
           <Text strong>Параметр</Text>
-          <Select size="large" className="symptoms-page__selector">
+          <Select size="large" className="symptoms-page__selector" value={param} onChange={e => setParam(e)}>
             {params.map(item => (
               <Option key={item} value={item}>
                 {item}
               </Option>
             ))}
-            <Option value="Yiminghe">yiminghe</Option>
           </Select>
         </div>
-        <div className="symptoms-page__selector-container-item">
-          <Text strong>Где находится</Text>
-          <Select size="large" className="symptoms-page__selector">
-            {places.map(item => (
-              <Option key={item} value={item}>
-                {item}
-              </Option>
-            ))}
-          </Select>
-        </div>
-        <div className="symptoms-page__selector-container-item">
-          <Text strong>Вид боли</Text>
-          <Select size="large" className="symptoms-page__selector">
-            {degrees.map(item => (
-              <Option key={item} value={item}>
-                {item}
-              </Option>
-            ))}
-          </Select>
-        </div>
-        <div className="symptoms-page__selector-container-item">
-          <Text strong>Степень боли</Text>
-          <Slider style={{ width: '100%' }} max={10} />
-        </div>
+        {param === 'Артериальное давление' || param === 'Температура' ? (
+          <div className="symptoms-page__selector-container-item">
+            <Text strong>Значение</Text>
+            <Input size="large"></Input>
+          </div>
+        ) : (
+          <>
+            <div className="symptoms-page__selector-container-item">
+              <Text strong>Где находится</Text>
+              <Select size="large" className="symptoms-page__selector">
+                {places.map(item => (
+                  <Option key={item} value={item}>
+                    {item}
+                  </Option>
+                ))}
+              </Select>
+            </div>
+            <div className="symptoms-page__selector-container-item">
+              <Text strong>Вид боли</Text>
+              <Select size="large" className="symptoms-page__selector">
+                {degrees.map(item => (
+                  <Option key={item} value={item}>
+                    {item}
+                  </Option>
+                ))}
+              </Select>
+            </div>
+            <div className="symptoms-page__selector-container-item">
+              <Text strong>Степень боли</Text>
+              <Slider style={{ width: '100%' }} max={10} />
+            </div>
+          </>
+        )}
       </div>
       <div className="symptoms-page__button-container">
         <Button type="primary" size={'large'} block>
