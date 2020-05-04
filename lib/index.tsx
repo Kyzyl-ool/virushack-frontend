96305652f021368e20df8b2bc25f911b66c24413
Kyzyl-ool Kezhik <kyzyloolk@mail.ru>
2020-05-05 01:15:51 +0300

    Revert "no firebase"
    This reverts commit 4d1c3e9fcdcf8eebbd496168d227cec2b6925ccb.

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
 
-// if ('serviceWorker' in navigator) {
-//   navigator.serviceWorker
-//     .register('./firebase-messaging-sw.js')
-//     .then(function(registration) {
-//       console.log('Registration successful, scope is:', registration.scope);
-//     })
-//     .catch(function(err) {
-//       console.log('Service worker registration failed, error:', err);
-//     });
-// }
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('./firebase-messaging-sw.js')
+    .then(function(registration) {
+      console.log('Registration successful, scope is:', registration.scope);
+    })
+    .catch(function(err) {
+      console.log('Service worker registration failed, error:', err);
+    });
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
